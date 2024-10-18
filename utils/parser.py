bd21111952c3f63b475d4b51bf80b509d9b65bac
lalunex <63094854+lalunex@users.noreply.github.com>
2024-10-18 19:17:16 +0800

    Add files via upload

---
+++ utils/parser.py
@@ -0,0 +1,47 @@
+'''
+Author: Yidan Liu 1334252492@qq.com
+Date: 2024-02-06 12:20:53
+LastEditors: Yidan Liu 1334252492@qq.com
+LastEditTime: 2024-10-18 18:44:33
+FilePath: /add_noise/utils/parser.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--model_name', default='MGSD', type=str, help="model selection"
+                                                                      "you can choose one of ['MGSD', 'GRU4Rec', 'NARM', 'Caser', 'BERT4Rec', 'SASRec', 'STAMP']")
+    parser.add_argument('--dataset', default='ml-1m', type=str, help="Dataset to use"
+                                                                       "you can choose one of ml-100k, ml-1m, amazon-beauty, amazon-sports-outdoors, yelp")
+    parser.add_argument('--sub_model', default='BERT4Rec', type=str, help='sub-model selection'
+                                                                               "you can choose one of ['GRU4Rec', 'NARM', 'Caser', 'BERT4Rec', 'SASRec', 'STAMP']")
+    # parser.add_argument('--local_rank', default=0, type=int,
+    #                     help='node rank for distributed training')
+    parser.add_argument('--seed', default=2024, type=int, help='seed for experiment')
+    parser.add_argument('--verbose', default=False, type=bool, help='whether save the log file')
+    parser.add_argument('--embedding_size', default=100, type=int, help='embedding size for all layer')
+    parser.add_argument('--learning_rate', default=1e-3, type=float, help='learning rate')
+    parser.add_argument('--weight_decay', default=0, type=float, help="weight decay for adam optimizer")
+    parser.add_argument('--train_batch_size', default=256, type=int, help='train batch size')
+    parser.add_argument('--hard_noise_num', default=5, type=int, help='add hard noise ratio')
+    parser.add_argument('--user_interest_num', default=5, type=int, help='user interest num')
+    parser.add_argument('--gaussian_kernel_num', default=10, type=int, help='gaussian kernel num')
+    parser.add_argument('--is_gumbel_tau_anneal', default=True, type=bool, help='whether use anneal for gumbel')
+    parser.add_argument('--gumbel_temperature', default=0.5, type=float, help='gumbel temperature')
+    parser.add_argument('--is_spu_cl_tau_anneal', default=True, type=bool, help='whether use anneal for gumbel')
+    parser.add_argument('--supervised_contrastive_temperature', default=0.5, type=float, help='gumbel temperature')
+    parser.add_argument('--our_ae_drop_out', default=0.1, type=float, help='our average drop-out rate')
+    parser.add_argument('--load_pre_train_emb', default=False, type=bool, help='whether load pre-train model embedding')
+    parser.add_argument('--reweight_loss_gamma', default=10, type=float, help='reweight loss alpha for ave') # 1-5
+    parser.add_argument('--reweight_loss_zeta', default=7, type=float, help='reweight loss lambda for gaussian') # 1-10
+    parser.add_argument('--reweight_loss_eta', default=1, type=float, help='reweight loss lambda for gaussian') # 1-20
+    parser.add_argument('--gpu_id', default=0, type=int, help='gpu id')
+    parser.add_argument('--warm_up_ratio', default=0, type=int, help='warm up ratio for lr')
+
+    parser.add_argument('--train_set_ratio', default=1, type=float, help='train set ratio for model')
+
+    args = parser.parse_args()
+    return args
---
+++ utils/utils.py
@@ -0,0 +1,83 @@
+'''
+Author: Yidan Liu 1334252492@qq.com
+Date: 2024-02-06 12:20:53
+LastEditors: Yidan Liu 1334252492@qq.com
+LastEditTime: 2024-10-18 18:44:57
+FilePath: /add_noise/utils/utils.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import importlib
+import math
+import numpy as np
+
+
+def get_model(model_name):
+    model_file_name = model_name.lower()
+    module_path = '.'.join(['model', model_file_name])
+    if importlib.util.find_spec(module_path, __name__):
+        model_module = importlib.import_module(module_path, __name__)
+    else:
+        raise ValueError('`model_name` [{}] is not the name of an existing model.'.format(model_name))
+
+    model_class = getattr(model_module, model_name)
+
+    return model_class
+
+
+def is_mgsd_model(model_name):
+    return 'MGSD' in model_name
+
+
+def compute_indicator(predict, ground_true):
+    Hits_i = 0
+    MRR_i = 0
+    HR_i = 0
+    NDCG_i = 0
+    for i in range(len(predict)):
+        for j in range(len(predict[i])):
+            if ground_true[i][0]==predict[i][j]:
+                Hits_i+=1
+                HR_i+=1
+                # 注意j的取值从0开始
+                MRR_i+=1/(j+1)   
+                NDCG_i+=1/(math.log2(1+j+1))
+                break
+    return HR_i, NDCG_i, MRR_i
+
+def indicators_20(predict, ground_true, seq_length):
+    different_seq_res = {}
+    Len_T = len(ground_true)
+
+    # ml-100k, 序列长度为18-50，所以分为0-28， 29-49， 50
+    # beauty, 序列长度为4-50，所以分为4， 5-6， 7-50
+    # sports，所以分为4， 5-6， 7-50
+    # yelp, 所以分为4， 5-7， 8-50
+    # ml-1m, 所以分为0-54， 55-167， 167-200
+
+    # ml-100k, 序列长度为18-50，所以分为0-49， 50
+    # beauty, 序列长度为4-50，所以分为4-5， 6-50
+    # sports，所以分为4-5， 6-50
+    # yelp, 所以分为4， 5-7， 8-50
+    # ml-1m, 所以分为0-54， 55-167， 167-200
+    short_predict_seq = predict[np.where(seq_length <= 28)]
+    short_ground_true = np.expand_dims(ground_true[np.where(seq_length <= 28)], axis=-1)
+    HR_s, NDCG_s, MRR_s = compute_indicator(short_predict_seq, short_ground_true)
+    different_seq_res['short_HR_20'] = HR_s / Len_T
+    different_seq_res['short_NDCG_20'] = NDCG_s / Len_T
+    different_seq_res['short_MRR_20'] = MRR_s / Len_T
+
+    medium_predict_seq = predict[np.where((seq_length >= 29) &  (seq_length <= 49))]
+    medium_ground_true = np.expand_dims(ground_true[np.where((seq_length >= 29) & (seq_length <= 49))], axis=-1)
+    HR_m, NDCG_m, MRR_m = compute_indicator(medium_predict_seq, medium_ground_true)
+    different_seq_res['medium_HR_20'] = HR_m / Len_T
+    different_seq_res['medium_NDCG_20'] = NDCG_m / Len_T
+    different_seq_res['medium_MRR_20'] = MRR_m / Len_T
+
+    long_predict_seq = predict[np.where(seq_length >= 50)]
+    long_ground_true = np.expand_dims(ground_true[np.where(seq_length >= 50)], axis=-1)
+    HR_l, NDCG_l, MRR_l = compute_indicator(long_predict_seq, long_ground_true)
+    different_seq_res['long_HR_20'] = HR_l / Len_T
+    different_seq_res['long_NDCG_20'] = NDCG_l / Len_T
+    different_seq_res['long_MRR_20'] = MRR_l / Len_T
+
+    return different_seq_res
